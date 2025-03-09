91f5def2050186584c88d277104be67521774906
Mohamad Umar M <81101575+mdumar2430@users.noreply.github.com>
2025-03-09 21:23:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const client = new Client({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 client.connect();
 
